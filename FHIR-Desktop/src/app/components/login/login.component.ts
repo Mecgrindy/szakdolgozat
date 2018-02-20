470e57fecf9505a0810bc603e4133c7b12bc5c11
Gracijan <grindy2015@gmail.com>
2018-02-20 21:34:12 +0100

    Registration and login validation.

--- FHIR-Desktop/src/app/components/login/login.component.ts
+++ FHIR-Desktop/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
+  logForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.logForm = new FormGroup({
+      'email': new FormControl(this.email, [
+        Validators.required,
+        Validators.email
+      ]),
+      'password': new FormControl(this.password, [Validators.required])
+    });
+  }
+  getErrorMessage(el: any) {
+    return el.hasError('required') ? 'You must enter a value' :
+      el.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+  onSubmit() {
+    console.log(this.logForm);
   }
 
 }
--- FHIR-Desktop/src/app/components/registration/registration.component.ts
+++ FHIR-Desktop/src/app/components/registration/registration.component.ts
@@ -1,4 +1,6 @@
+import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  email: string;
+  password: string;
+  rePassword: string;
+  regForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.regForm = new FormGroup({
+      'email': new FormControl(this.email, [
+        Validators.required,
+        Validators.email
+      ]),
+      'password': new FormControl(this.password, [Validators.required]),
+      'rePassword': new FormControl(this.rePassword, Validators.required)
+    });
+  }
+
+  getErrorMessage(el: any) {
+    return el.hasError('required') ? 'You must enter a value' :
+      el.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+  onSubmit() {
+    console.log(this.regForm);
   }
 
 }
+
+/*export class PasswordValidation {
+
+  static MatchPassword(AC: AbstractControl) {
+    const password = AC.get('password').value; // to get value in input tag
+    const confirmPassword = AC.get('confirmPassword').value; // to get value in input tag
+    if (password !== confirmPassword) {
+      console.log('false');
+      AC.get('confirmPassword').setErrors({ MatchPassword: true });
+    } else {
+      console.log('true');
+      return null;
+    }
+  }
+}*/

5a2921c0ef8cfc957b2354ab2aa81639a931a9a0
Gracijan <grindy2015@gmail.com>
2018-02-28 17:41:31 +0100

    További módosítások a funkspecben.

--- FHIR-Desktop/src/app/components/home/home.component.ts
+++ FHIR-Desktop/src/app/components/home/home.component.ts
@@ -21,25 +21,25 @@ export class HomeComponent {
 
   states: State[] = [
     {
-      name: 'Arkansas',
+      name: 'Aladin',
       population: '2.978M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
     },
     {
-      name: 'California',
+      name: 'Conor',
       population: '39.14M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
     },
     {
-      name: 'Florida',
+      name: 'Marika',
       population: '20.27M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
     },
     {
-      name: 'Texas',
+      name: 'Gyula',
       population: '27.47M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
